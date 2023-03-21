a6a85aa1c72dacbbd3bbb66126c29de21a2640d4
Vialli Gonsalves <gonsalvesvialli06@gmail.com>
2023-03-21 11:12:33 +0530

    changes

--- wp-content/themes/newtheme/js/script.js
+++ wp-content/themes/newtheme/js/script.js
@@ -1,8 +1,18 @@
-// $(document).ready(function() {
-//     alert('test');
-// });
 
-        
+let videoList = document.querySelectorAll('.video-list-container .list');
+
+videoList.forEach(vid =>{
+   vid.onclick = () =>{
+      videoList.forEach(remove =>{remove.classList.remove('active')});
+      vid.classList.add('active');
+      let src = vid.querySelector('.list-video').src;
+      let title = vid.querySelector('.list-title').innerHTML;
+      document.querySelector('.main-video-container .main-video').src = src;
+      document.querySelector('.main-video-container .main-video').play();
+      document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
+   };
+});
+
 let month = document.querySelector('.price-toggler .month');
 let year = document.querySelector('.price-toggler .year');
 let monthAmount = document.querySelectorAll('.box-container .box .month');
@@ -23,3 +33,25 @@ month.onclick = () =>{
     monthAmount.forEach(mo =>{ mo.style.display = 'block' });
     yearAmount.forEach(yr =>{ yr.style.display = 'none' });
 };
+
+const swiper = new Swiper('.swiper', {
+            
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction:false,
+            },
+            loop: true,
+        
+            pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            },
+        
+            
+            navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+            },
+        
+            
+        });
